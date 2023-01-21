d60b6f347902917fe61d3b5cfca5168258280f82
pranjal9938 <123217601+pranjal9938@users.noreply.github.com>
2023-01-21 06:36:41 +0530

    Update data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,39 +1,39 @@
 const data = [
     {
         roomName: "PoolSide",
-        roomLink : "http://192.168.1.7/poolsideroom",
+        roomLink : "http://192.168.1.14/poolsideroom",
     },
     {
         roomName: "Kitchen Room",
-        roomLink : "http://192.168.1.7/kitchenroom",
+        roomLink : "http://192.168.1.14/kitchenroom",
     },
     {
         roomName: "Bed Room",
-        roomLink : "http://192.168.1.7/bedroom",
+        roomLink : "hhttp://192.168.1.14/bedroom",
     },
     {
         roomName: "Stairs",
-        roomLink : "http://192.168.1.7/stairroom",
+        roomLink : "http://192.168.1.14/stairroom",
     },
     {
         roomName: "Party Room",
-        roomLink : "http://192.168.1.7/partyroom",
+        roomLink : "http://192.168.1.14/partyroom",
     },
     {
         roomName: "Dining Room",
-        roomLink : "http://192.168.1.7/diningroom",
+        roomLink : "http://192.168.1.14/diningroom",
     },
     {
         roomName: "Swimming Pool",
-        roomLink : "http://192.168.1.7/swimmingpoolroom",
+        roomLink : "http://192.168.1.14/swimmingpoolroom",
     },
     {
         roomName :"Fan",
-        roomLink : "192.168.1.7/fan"
+        roomLink : "http://192.168.1.14/fan"
     },
     {
         roomName :"Gate",
-        roomLink : "192.168.1.7/gateopen"
+        roomLink : "http://192.168.1.14/gateopen"
     }
 ]
 
